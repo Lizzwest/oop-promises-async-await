89c6aaf776f89d645fa58add9bf4235aee8e6292
Lizz <lizwesterband@gmail.com>
2020-07-23 14:46:54 -0700

    [functions] Add constructor functions

--- app.js
+++ app.js
@@ -35,3 +35,49 @@ console.log(carTwo);
 
 
 //constructor functions
+
+function User(name){
+    //create this variable; sets this equal to this user
+    this.name = name;
+//return this
+}
+
+
+
+
+
+let lizz = new User("Lizz");
+let pete = new User("pete")
+
+
+console.log(lizz.name)
+console.log(pete.name)
+
+function tvShows( name,stars, genre){
+    this.name = name;
+    this.stars = stars;
+    this.genre = genre;
+
+}
+
+let tgp= new tvShows("the Good Place", "Kristen Bell", "Comedy")
+
+
+function broadwayStar(name, show, role){
+    this.name = name;
+    this.show = show;
+    this.role = role;
+
+}
+
+let idina = new broadwayStar("Idina Menzel", "Wicked", "Elphaba");
+let kristin = new broadwayStar("Kristin Chenowith","Wicked", "Galinda with a Ga" );
+let ben = new broadwayStar("Ben Platt", "Dear Evan Hansen", "Evan Hansen")
+console.log(idina)
+console.log(idina.name);
+console.log(kristen)
+console.log(ben)
+
+
+
+
